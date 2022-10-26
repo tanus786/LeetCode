bc1a73344d164b0b202513c21343a5ead3abc2a7
Tanu soni <Tanusoni2312@gmail.com>
2022-10-26 14:00:18 +0530

    Time: 1447 ms (6.31%), Space: 47.5 MB (24.20%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,7 @@
-class Solution {
+public class Solution {
     public boolean isPalindrome(String s) {
-        String str,rev="";
-        str = s.replaceAll("[^a-zA-Z0-9]", "");
-        str =str.toLowerCase();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev = rev + str.charAt(i);
-        }
-        return str.equals(rev);
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
     }
 }
