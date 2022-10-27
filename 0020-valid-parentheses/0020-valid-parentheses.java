44b10df5e63be6f61fe67e3e5f331a5bb4db4a3c
Tanu soni <Tanusoni2312@gmail.com>
2022-10-27 11:17:10 +0530

    Time: 3 ms (77.29%), Space: 42.4 MB (14.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,11 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-            while (s.indexOf("{}") !=-1 || s.indexOf("[]") !=-1 || s.indexOf("()") !=-1 ){
-        s=s.replace("()","");
-        s=s.replace("[]","");
-        s=s.replace("{}","");
-        
+        HashMap map = new HashMap();
+        map.put('(',')');
+        map.put('[',']');
+        map.put('{','}');
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0;i < s.length();i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(map.get(stack.pop()).equals(c)){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
-    return s.isEmpty();
-}
+
 }
