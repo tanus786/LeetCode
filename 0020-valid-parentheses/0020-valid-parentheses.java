2fba748552231523c18cbc17513347583346bbde
Tanu soni <Tanusoni2312@gmail.com>
2022-10-27 11:17:33 +0530

    Time: 3 ms (77.29%), Space: 42.4 MB (20.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-        HashMap map = new HashMap();
-        map.put('(',')');
-        map.put('[',']');
-        map.put('{','}');
-        Stack<Character> stack = new Stack<>();
-
-        for(int i = 0;i < s.length();i++){
-            char c = s.charAt(i);
-            if(c == '(' || c == '{' || c == '['){
-                stack.push(c);
-            }else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                if(map.get(stack.pop()).equals(c)){
-                    continue;
-                }else{
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
+        Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
     }
 
 }
