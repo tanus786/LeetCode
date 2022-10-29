198021a20b4660984a92d4cce0642c69110187ab
Tanu soni <Tanusoni2312@gmail.com>
2022-10-29 11:39:01 +0530

    Time: 906 ms (5.00%), Space: 118 MB (5.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,9 +1,9 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int max=1;
-if(s.contains("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")){
-            return 95;
-        }
+// if(s.contains("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")){
+//             return 95;
+//         }
         // if(s.contains(" ")){
         //     return 1;
         // }
@@ -16,7 +16,6 @@ if(s.contains("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"
         for(int i = 1,j=1;i<s.length() && j<s.length();i++){
             if(str.contains((""+s.charAt(i))) == false){
                 str+=s.charAt(i);
-                System.out.println(str);
                 max = Math.max(max,str.length());
             }
             else{
