b50602f9eb97af0d9de6743af4476da7f53d1eea
Tanu soni <Tanusoni2312@gmail.com>
2022-11-14 00:39:17 +0530

    Create Largest Odd Number in String.java

---
+++ Largest Odd Number in String.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        for(int i=num.length() - 1; i>=0; i--)
+		if(num.charAt(i) % 2 != 0) return num.substring(0, i + 1);
+	return "";
+    }
+}
