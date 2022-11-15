366a2e81f8ddfcd0695e3242750546246ec69465
Tanu soni <Tanusoni2312@gmail.com>
2022-11-16 00:51:17 +0530

    Create 507. Perfect Number.java

---
+++ 507. Perfect Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        if(num % 2 != 0){
+            return false;
+        }
+        if(num<1) return false;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0)
+                sum+=i;
+        }
+        return sum == num;
+    }
+}
