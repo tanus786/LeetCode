7032ea1f1e53aae6a1f6166aa45ce34f26365799
Tanu soni <Tanusoni2312@gmail.com>
2022-11-27 14:31:00 +0530

    Create 974. Subarray Sums Divisible by K.java

---
+++ 974. Subarray Sums Divisible by K.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum % k == 0) res++;
+            }
+        }
+        return res;
+    }
+}
