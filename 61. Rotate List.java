a079ac95d6b62175b8ce74522f9f4459f448e2f1
Tanu soni <Tanusoni2312@gmail.com>
2022-12-06 18:39:23 +0530

    Create 61. Rotate List.java

---
+++ 61. Rotate List.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null)
+        return null;
+	int size = 1; // since we are already at head node
+	ListNode fast=head;
+	ListNode slow = head;
+	
+	while(fast.next!=null){
+	    size++;
+	    fast = fast.next;
+	}
+	for(int i=size-k%size;i>1;i--) // i>1 because we need to put slow.next at the start.
+		slow = slow.next;
+	fast.next = head;
+	head = slow.next;
+	slow.next = null;
+	
+	return head;
+    }
+}
