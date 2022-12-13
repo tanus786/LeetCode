7c36cc1846fe0d9de69eab28dfc1f7e2431c2cdb
Tanu soni <Tanusoni2312@gmail.com>
2022-12-14 00:03:52 +0530

    Create 746. Min Cost Climbing Stairs.java

---
+++ 746. Min Cost Climbing Stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        int m = 0, n = 0, k = 0;
+        for(int i = 2; i <= len; i++) {
+            k = Math.min(m+cost[i-2], n+cost[i-1]);
+            m = n;
+            n = k;
+        }
+        return k;
+
+    }
+}
