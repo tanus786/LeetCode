6a453d907dc35e6cda962721a274c646b1b859d6
Tanu soni <Tanusoni2312@gmail.com>
2022-12-16 13:19:46 +0530

    Update 225. Implement Stack using Queues.java

--- 225. Implement Stack using Queues.java
+++ 225. Implement Stack using Queues.java
@@ -20,3 +20,39 @@ class MyStack {
         return mainStack.isEmpty();
     }
 }
+
+
+
+## solution 2nd
+
+class MyStack {
+    Queue<Integer> realQueue = new LinkedList<>();
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        Queue<Integer> temp = new LinkedList<>();
+        while(!realQueue.isEmpty())
+        {
+            temp.add(realQueue.remove());
+        }
+        realQueue.add(x);
+        while(!temp.isEmpty())
+        {
+          realQueue.add(temp.remove());  
+        }
+    }
+    
+    public int pop() {
+        return realQueue.remove();
+    }
+    
+    public int top() {
+        return realQueue.peek();
+    }
+    
+    public boolean empty() {
+        return realQueue.isEmpty();
+    }
+}
